261c1e31767cf5f170f66677a74922bf14ac5a5c
zuhaib.amjad <umar0423@gmail.com>
2025-11-17 00:34:54 +0500

    Run server

--- server.ts
+++ server.ts
@@ -1,1 +1,12 @@
-console.log("Yo bro whats up")
+import app from "./src/app.js"
+
+const startServer = () => {
+  const port = process.env.PORT || 3000;
+
+  app.listen(port, () => {
+    console.log("Listening on this port: ", port)
+  })
+}
+
+
+startServer()
