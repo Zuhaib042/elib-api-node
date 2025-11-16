89f93d56fb52956246c3205b51101fa66f03974d
zuhaib.amjad <umar0423@gmail.com>
2025-11-17 00:36:13 +0500

    prettify code

--- server.ts
+++ server.ts
@@ -1,12 +1,11 @@
-import app from "./src/app.js"
+import app from "./src/app.js";
 
 const startServer = () => {
   const port = process.env.PORT || 3000;
 
   app.listen(port, () => {
-    console.log("Listening on this port: ", port)
-  })
-}
+    console.log("Listening on this port: ", port);
+  });
+};
 
-
-startServer()
+startServer();
